89e1aefc73dbbe6c9bb7f1fac85b9ae2f76b4f46
Ayush Kumbhalkar <ayushkumbhalkar68@gmail.com>
2025-06-06 13:14:08 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
-// const mongoURI = "mongodb://localhost:27017/inotebook";
-const mongoURI =
-  "mongodb+srv://ayushkumbhalkar68:sDAlBWgZEn18TQKy@ayushcluster0.i6hkgm7.mongodb.net/?retryWrites=true&w=majority&appName=AyushCluster0";
+
+const mongoURI = process.env.MONGO_URI;
 
 const connectToMongo = () => {
-  mongoose.connect(mongoURI);
+  mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
 };
 
 module.exports = connectToMongo;
+
