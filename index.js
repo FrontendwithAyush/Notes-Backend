b4420b7d70f2e26525ba97fc412ccd01d5326062
Ayush Kumbhalkar <ayushkumbhalkar68@gmail.com>
2025-06-06 13:13:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const connectToMongo = require("./db");
 const express = require("express");
-var cor = require("cors");
+var cors = require("cors");
 
 connectToMongo();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.use(cor());
+app.use(cors());
 app.use(express.json());
 //available routes
 app.use("/api/auth", require("./routes/auth"));
